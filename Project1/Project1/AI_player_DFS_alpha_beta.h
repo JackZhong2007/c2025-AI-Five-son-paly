c14cb384b4874a5eaa3b5533298e74652e424ede
JackZhong2007 <2979605558@qq.com>
2025-12-15 20:41:29 +0800

    TEST_28:raylib实现后的重构——使begindrawing和enddrawing之间只进行绘图,将其他逻辑放在外面；修补了一些逻辑不恰当的Bug;调正了一下开头引用外部source部分的位置；增加AI落子提示和人类落子提示;解决了使用DrawText时的警告问题（改为使用DrawTextEx）

--- Project1/Project1/AI_player_DFS_alpha_beta.h
+++ Project1/Project1/AI_player_DFS_alpha_beta.h
@@ -1,7 +1,7 @@
 #pragma once
 #include"head_title_library.h"
 #include"evaluate_score.h"
-int has_neighbor(SPACE chessboard[LENGTH][LENGTH], int x, int y, int distance);int minimax(SPACE chessboard[LENGTH][LENGTH], int depth, int alpha, int beta, int maximizingPlayer, int color);
+int has_neighbor(SPACE chessboard[LENGTH][LENGTH], int x, int y, int distance);
 int minimax(SPACE chessboard[LENGTH][LENGTH], int depth, int alpha, int beta, int maximizingPlayer, int color);
 int situation_assessment(SPACE chessboard[LENGTH][LENGTH], int color);
 SPACE AI_player_optimized(SPACE chessboard[LENGTH][LENGTH], int color);
--- Project1/Project1/main.c
+++ Project1/Project1/main.c
@@ -2,15 +2,14 @@
 #include"victory_condition.h"
 #include"AI_player_DFS_alpha_beta.h"
 int main() {
-	//TEST_27.2:raylib实现后的重构——两个界面界面纹路位置调整
+	//TEST_28:raylib实现后的重构——使begindrawing和enddrawing之间只进行绘图,将其他逻辑放在外面；修补了一些逻辑不恰当的Bug;调正了一下开头引用外部source部分的位置；增加AI落子提示和人类落子提示;解决了使用DrawText时的警告问题（改为使用DrawTextEx）
 	srand((unsigned int)time(NULL));
 	InitAudioDevice();
 	Image boardImage = LoadImage("./source/OIP.jpg");
 	Image ruleImage = LoadImage("./source/rule.jpg");
 	Image blackImage = LoadImage("./source/black.jpg");
 	Image whiteImage = LoadImage("./source/white.jpg");
-	InitWindow(boardImage.width*5, boardImage.height*5, "AI_five_son_play");
-	SetTargetFPS(120);
+	InitWindow(boardImage.width * 5, boardImage.height * 5, "AI_five_son_play");
 	Texture BoardImage = LoadTextureFromImage(boardImage);
 	Texture RuleImage = LoadTextureFromImage(ruleImage);
 	Texture BlackImage = LoadTextureFromImage(blackImage);
@@ -19,9 +18,12 @@ int main() {
 	Vector2 rulePosition = { 0.0f, 50.0f };
 	Music bgm = LoadMusicStream("./source/music.mp3");
 	Sound sound = LoadSound("./source/sound2.wav");
+	Font font = GetFontDefault();
+	Color tint = { 255, 100, 50, 255 };
 	SetMusicVolume(bgm, 0.5f);
 	SetSoundVolume(sound, 10.0f);
 	PlayMusicStream(bgm);
+	SetTargetFPS(120);
 	int color = rand() % 2;
 	SPACE chessboard[LENGTH][LENGTH] = { 0 };
 	for (int i = 0; i < LENGTH; i++) {
@@ -31,9 +33,12 @@ int main() {
 			chessboard[i][j].belong = Blank;
 		}
 	}
+	int AI_Column=0, AI_Line=0;
+	bool AI_moved = false;
 	if (color == White) {
-		int x_rand = rand() % 3 + 5, y_rand = rand() % 3 + 5;
-		chessboard[x_rand][y_rand].belong = Black;
+		AI_Column = rand() % 3 + 5, AI_Line = rand() % 3 + 5;
+		chessboard[AI_Line][AI_Column].belong = Black;
+		AI_moved = true;
 	}
 	int AI_color = (color == White) ? Black : White;
 	Color col= (color == White) ? WHITE : BLACK;
@@ -42,7 +47,6 @@ int main() {
 	int x=0, y=0;
 	bool left_button_pressed = false;
 	while (!WindowShouldClose()) {
-
 		if (IsKeyDown(KEY_ESCAPE)) {
 			break;
 		}
@@ -59,9 +63,33 @@ int main() {
 				current_state = (color == Black) ? BLACK_UI : WHITE_UI;
 				break;
 			case BLACK_UI:
+				current_state = GAMETIME;
+				break;
 			case WHITE_UI:
 				current_state = GAMETIME;
 				break;
+			}
+		}
+		if (current_state == GAMETIME && IsMouseButtonPressed(MOUSE_RIGHT_BUTTON) && left_button_pressed) {
+			if (line >= 0 && line < LENGTH && column >= 0 && column < LENGTH) {
+				if (chessboard[line][column].belong == Blank) {
+					chessboard[line][column].belong = color;
+					PlaySound(sound);
+					if (color == victory_condition(chessboard)) {
+						current_state = WIN_UI;
+					}
+					else {
+						SPACE AI = AI_player_optimized(chessboard, AI_color);
+						AI_Line = AI.x, AI_Column = AI.y;
+						if (AI_Line >= 0 && AI_Line < LENGTH && AI_Column >= 0 && AI_Column < LENGTH) {
+							chessboard[AI_Line][AI_Column].belong = AI_color;
+							AI_moved = true;
+						}
+						if (AI_color == victory_condition(chessboard)) {
+							current_state = LOSE_UI;
+						}
+					}
+				}
 			}
 		}
 		BeginDrawing();
@@ -79,6 +107,7 @@ int main() {
 			DrawTextureEx(BoardImage, boardPosition, 0.0f, 5.0f, WHITE);
 			if (line >= 0 && line < LENGTH && column >= 0 && column < LENGTH) {
 				DrawCircle(60 + 92 * column, 120 + 92 * line, 40, col);
+				DrawCircle(60 + 92 * column, 120 + 92 * line, 8, RED);
 			}
 			for (int i = 0; i < LENGTH; i++) {
 				for (int j = 0; j < LENGTH; j++) {
@@ -90,32 +119,15 @@ int main() {
 					}
 				}
 			}
-			if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON) && left_button_pressed) {
-				if (line >= 0 && line < LENGTH && column >= 0 && column < LENGTH) {
-					if (chessboard[line][column].belong == Blank) {
-						chessboard[line][column].belong = color;
-						PlaySound(sound);
-						if (color == victory_condition(chessboard)) {
-							current_state = WIN_UI;
-						}
-						else {
-							SPACE AI = AI_player_optimized(chessboard, AI_color);
-							if (AI.x >= 0 && AI.x < LENGTH && AI.y >= 0 && AI.y < LENGTH) {
-								chessboard[AI.x][AI.y].belong = AI_color;
-							}
-							if (AI_color == victory_condition(chessboard)) {
-								current_state = LOSE_UI;
-							}
-						}
-					}
-				}
+			if (AI_moved) {
+				DrawCircle(60 + 92 * AI_Column, 120 + 92 * AI_Line, 8, RED);
 			}
 			break;
 		case WIN_UI:
-			DrawText("You win!Congratulations!", rulePosition.x, rulePosition.y, 115, BLACK);
+			DrawTextEx(font,"You win!Congratulations!",rulePosition, 160.0f, 4.0f, tint);
 			break;
 		case LOSE_UI:
-			DrawText("You lose!What a pity!", rulePosition.x, rulePosition.y, 130, BLACK);
+			DrawTextEx(font, "You fail!What a pity!", rulePosition, 160.0f, 4.0f, tint);
 			break;
 		}
 		EndDrawing();
